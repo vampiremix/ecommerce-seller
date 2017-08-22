02140d0722fff5f7d3e1bf9b7ca31e1e456daae4
moomam6934 <destinationpainbm@gmail.com>
2017-08-22 18:12:58 +0700

    TabsNavigationPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make WalkthroughPage the root (or first) page
-  rootPage: any = HomeManageOrderPage;
+  rootPage: any = TabsNavigationPage;
   
   textDir: string = "ltr";
 
