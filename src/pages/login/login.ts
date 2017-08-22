5556054ce18b1720676d4b78c353ae074e3dcad3
Nutprapobsun Thongchantr <nutprapobsun.mick@gmail.com>
2017-08-22 14:56:09 +0700

    Mick Login page & Add Json File

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { UserProvider } from '../../providers/user/user';
 import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
@@ -5,6 +6,8 @@ import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { SignupPage } from '../signup/signup';
 import { ForgotPasswordPage } from '../forgot-password/forgot-password';
 import { TabsNavigationPage } from '../tabs-navigation/tabs-navigation';
+
+import { LoginService } from "./login.service";
 
 // import { FacebookLoginService } from '../facebook-login/facebook-login.service';
 // import { GoogleLoginService } from '../google-login/google-login.service';
@@ -25,20 +28,53 @@ export class LoginPage {
     // public facebookLoginService: FacebookLoginService,
     // public googleLoginService: GoogleLoginService,
     // public twitterLoginService: TwitterLoginService,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    private loginSer: LoginService,
+    private userpro: UserProvider
   ) {
     this.main_page = { component: TabsNavigationPage };
 
     this.login = new FormGroup({
-      email: new FormControl('', Validators.required),
-      password: new FormControl('test', Validators.required)
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
     });
   }
 
-  doLogin(){
-    this.nav.setRoot(this.main_page.component);
+  doLogin() {
+    // let loading = this.loadingCtrl.create({
+    //   content: 'Please wait...'
+    // });
+    // loading.present();
+    // let user = { "username": this.login.value.username, "password": this.login.value.password }
+    // this.loginSer.logingin(user).then(data => {
+
+    //   this.userpro.user = data;
+    //   console.log(this.userpro.user);
+    //   loading.dismiss();
+    //   this.nav.setRoot(this.main_page.component);
+    // }).catch(err => {
+    //   alert("Username or password is not correct");
+    //   loading.dismiss();
+    // });
+
+    // Loogin Local
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loading.present();
+    let user = { "username": this.login.value.username, "password": this.login.value.password }
+    this.loginSer.logingin(user).then(data => {
+      this.userpro.user = data;
+      console.log(this.userpro.user);
+      loading.dismiss();
+      this.nav.setRoot(this.main_page.component);
+    }).catch(err => {
+      alert("Username or password is not correct");
+      loading.dismiss();
+    });
+
   }
 
-  
+
 
 }
