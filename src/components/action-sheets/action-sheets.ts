639067378e4c0cc2a9a497e777451f31f13a5ff1
oraphan.poo@northbkk.ac.th <oraphaan.poo@northbkk.ac.th>
2017-08-22 16:21:23 +0700

    update action sheets

--- src/components/action-sheets/action-sheets.ts
+++ src/components/action-sheets/action-sheets.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform, ActionSheetController, AlertController } from "ionic-angular";
 
 /**
  * Generated class for the ActionSheetsComponent component.
@@ -12,11 +13,54 @@ import { Component } from '@angular/core';
 })
 export class ActionSheetsComponent {
 
-  text: string;
 
-  constructor() {
-    console.log('Hello ActionSheetsComponent Component');
-    this.text = 'Hello World';
+  constructor(
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController,
+    public alerCtrl: AlertController
+  ) {
+
+  }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      // title: 'Albums',
+      cssClass: 'action-sheets',
+      buttons: [
+        {
+          text: 'ดูรายละเอียดสินค้า',
+          // icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'แก้ไขสินค้า',
+          // icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'ลบสินค้า',
+          role: 'destructive',
+          // icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+            alert('ยืนยันการลบสินค้า!');            
+          }
+        },
+        {
+          text: 'ยกเลิก',
+          role: 'cancel', // will always sort to be on the bottom
+          // icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
---
+++ src/pages/manage-product/manage-product.model.ts
@@ -0,0 +1,10 @@
+export class ProdLists {
+    title: string;
+    product: Array<ProductLists>;
+}
+export class ProductLists {
+    name: string;
+    rate: number;
+    price: string;
+    img: string;
+}
