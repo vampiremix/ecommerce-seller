ea54949f75f0656a4bd289b9cd0ffb9821b21642
abhisit sungnark <abhisit2537@hotmail.com>
2017-08-24 10:39:37 +0700

    5 tabs commit

--- src/pages/tabs-navigation/tabs-navigation.ts
+++ src/pages/tabs-navigation/tabs-navigation.ts
@@ -1,3 +1,6 @@
+import { SettingUserPage } from '../setting-user/setting-user';
+import { SettingShopPage } from '../setting-shop/setting-shop';
+import { HistoryPage } from '../history/history';
 import { SettingPage } from '../setting/setting';
 import { ManageProductPage } from '../manage-product/manage-product';
 import { HomeManageOrderPage } from '../home-manage-order/home-manage-order';
@@ -12,10 +15,14 @@ export class TabsNavigationPage {
   tab1Root: any;
   tab2Root: any;
   tab3Root: any;
+  tab4Root: any;
+  tab5Root: any;
 
   constructor() {
     this.tab1Root = HomeManageOrderPage;
     this.tab2Root = ManageProductPage;
-    this.tab3Root = SettingPage;
+    this.tab3Root = HistoryPage;
+    this.tab4Root = SettingShopPage;
+    this.tab5Root = SettingPage;
   }
 }
